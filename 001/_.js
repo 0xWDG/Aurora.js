a4f56346ddf08a7e49e96c1da53e11f9bbec9c3c
Wesley De Groot <sales@wdgp.nl>
2015-04-08 20:59:41 +0200

    TYPO

--- 001/_.js
+++ 001/_.js
@@ -5,20 +5,19 @@
 	https://www.github.com/wesdegroot/_.js/
 	or https://www.wdgwv.com
 
-	Git:     https://www.github.com/wesdegroot/_.js
-	Todo: 	 https://www.github.com/wesdegroot/_.js/issues
+	Git:     https://github.com/wesdegroot/_.js
+	Todo: 	 https://github.com/wesdegroot/_.js/issues
 	Licence: https://github.com/wesdegroot/_.js/blob/master/LICENCE.md
 */
 
 // Anonymous function
 (function () {
-    // W returns new Library object that hold our selector. Ex: Q('.wrapper')
+    // _ returns new Library object that hold our selector. Ex: _('.wrapper')
     var _ = function (params) {
         return new Library(params);
     };
      
     // In our Library we get our selector with querySelectorAll
-    // We also add selector length, version and twitter/github or whatever you like as information about your library.
     var Library = function (params) {
         var selector = document.querySelectorAll(params),i = 0;
         // Get selector length
@@ -73,7 +72,7 @@
 	}
     };
     
-    // Assign our Q object to global window object.
+    // Assign our _ object to global window object.
     if(!window._) {
         window._ = _;
     }
