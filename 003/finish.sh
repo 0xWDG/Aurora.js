c50b11f1a443259097c9410070a435bd42c41251
Wesley de Groot <wes@vista.aero>
2015-09-11 22:41:47 +0200

    v0.0.3 Documentation

--- 003/finish.sh
+++ 003/finish.sh
@@ -1,16 +1,17 @@
+php GenerateDocs.php &>/dev/null
 # Generate sourcemapping files.
 for i in *.js; 
-	do java -jar /usr/local/bin/compiler.jar --js ./${i} --create_source_map ./m/${i}.map --js_output_file ./m/${i}; 
+	do java -jar /usr/local/bin/compiler.jar --js ./${i} --create_source_map ./m/${i}.map --js_output_file ./m/${i} &>/dev/null; 
 done
 
 # Add: //# sourceMappingURL=/path/to/file.js.map
 for i in *.js;
-	do sed -i -e '1i\'$'\n''//# sourceMappingURL='${i}'.map'$'\n' m/${i};
+	do sed -i -e '1i\'$'\n''//# sourceMappingURL='${i}'.map'$'\n' m/${i} &>/dev/null;
 done
 
-cd m
-rm *.js-e
-cd ..
+cd m &>/dev/null
+rm *.js-e &>/dev/null
+cd .. &>/dev/null
 
 # GOOD.
 /Applications/terminal-notifier.app/Contents/MacOS/terminal-notifier -appIcon "/Users/Wes/Cloud/WDGWV/Administatief/Logo's/logo.png" -contentImage "/Users/Wes/Cloud/Images/icons/-good.png" -title WDGWV -subtitle Success -message "Created Sourcemaps!" &> /dev/null
