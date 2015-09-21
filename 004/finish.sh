b129c934a0dd9800f3f28b70c2009804d19a6b16
Wesley de Groot <wes@vista.aero>
2015-09-21 20:01:18 +0200

    Auto-Pushing v0.0.4

--- 004/finish.sh
+++ 004/finish.sh
@@ -12,19 +12,23 @@ rm GenerateDocs.php &>/dev/null
 cp ../_.js_data/GenerateDocs.php GenerateDocs.php &>/dev/null
 
 # Generate index.html & generate wiki -> function list.
+echo "* Generating documentation..."
 php GenerateDocs.php &>/dev/null
 
+echo "* Generating map files"
 # Generate sourcemapping files.
 for i in *.js; 
 	do java -jar ../_.js_data/compiler.jar --js ./${i} --create_source_map ./m/${i}.map --js_output_file ./m/${i}; # Show errors ;D 
 done
 
+echo "* Fixing map files"
 # Add: //# sourceMappingURL=/path/to/file.js.map
 for i in *.js;
 	do sed -i -e '1i\'$'\n''//# sourceMappingURL='${i}'.map'$'\n' m/${i} &>/dev/null;
 done
 
 # remove temp files
+echo "* Removing temp files"
 cd m &>/dev/null
 rm *.js-e &>/dev/null
 cd .. &>/dev/null
