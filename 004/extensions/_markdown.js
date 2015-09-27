0c46de69bb7f2f93c169c6c676db1663ef237947
Wesley de Groot <wes@vista.aero>
2015-09-27 14:06:49 +0200

    Auto-Pushing v0.0.4

--- 004/extensions/_markdown.js
+++ 004/extensions/_markdown.js
@@ -13,31 +13,33 @@ else
         var len = this.length;
         while (len--) 
         {
-        	// Crazy markdown parser (demodemodemo)
-        	_('.md').hide(); // HIDE OLD ONE TEST MODE
+            // Crazy markdown parser (demodemodemo)
+            _('.md').hide(); // HIDE OLD ONE TEST MODE
+
             var el = document.createElement('script');
                 el.type = "text/javascript";
                 el.src  = "https://cdn.rawgit.com/google/code-prettify/master/loader/run_prettify.js";
 
             document.head.appendChild(el)
-    		var md       = document.getElementById('md-test');
-    		var mrk      = "";
-    		var codeOpen = false;
-    		var listOpen = false;
-    		// split all
-    		var parse    = this[len].innerHTML.split("\n");
+            var md       = document.getElementById('md-test');
+            var mrk      = "";
+            var codeOpen = false;
+            var listOpen = false;
 
-    		for (var i = 0; i < parse.length; i++)
-    		{
+            // split all
+            var parse    = this[len].innerHTML.split("\n");
+
+            for (var i = 0; i < parse.length; i++)
+            {
                 // Empty line (break)
-    			if (this.isEmpty(parse[i]))
-    			{
-    				mrk += "<br />";
-    			}
+                if (this.isEmpty(parse[i]))
+                {
+                    mrk += "<br />";
+                }
 
-    			// ##### H5
-    			else if (parse[i].substr(0,5) == "#####") 
-    			{
+                // ##### H5
+                else if (parse[i].substr(0,5) == "#####") 
+                {
                     if (listOpen)
                     {
                         mrk += "</ul>";
@@ -49,12 +51,12 @@ else
                         codeOpen = false;
                     }
 
-    				mrk += "<h5>" + parse[i].substr(5, parse[i].length) + "</h5>";
-    			}
+                    mrk += "<h5>" + parse[i].substr(5, parse[i].length) + "</h5>";
+                }
 
-    			// #### H4
-    			else if (parse[i].substr(0,4) == "####") 
-    			{
+                // #### H4
+                else if (parse[i].substr(0,4) == "####") 
+                {
                     if (listOpen)
                     {
                         mrk += "</ul>";
@@ -66,12 +68,12 @@ else
                         codeOpen = false;
                     }
 
-    				mrk += "<h4>" + parse[i].substr(4, parse[i].length) + "</h4>";
-    			}
+                    mrk += "<h4>" + parse[i].substr(4, parse[i].length) + "</h4>";
+                }
 
-    			// ### H3
-    			else if (parse[i].substr(0,3) == "###") 
-    			{
+                // ### H3
+                else if (parse[i].substr(0,3) == "###") 
+                {
                     if (listOpen)
                     {
                         mrk += "</ul>";
@@ -83,12 +85,12 @@ else
                         codeOpen = false;
                     }
 
-    				mrk += "<h3>" + parse[i].substr(3, parse[i].length) + "</h3>";
-    			}
+                    mrk += "<h3>" + parse[i].substr(3, parse[i].length) + "</h3>";
+                }
 
-    			// ## H2
-    			else if (parse[i].substr(0,2) == "##") 
-    			{
+                // ## H2
+                else if (parse[i].substr(0,2) == "##") 
+                {
                     if (listOpen)
                     {
                         mrk += "</ul>";
@@ -100,12 +102,12 @@ else
                         codeOpen = false;
                     }
 
-    				mrk += "<h2>" + parse[i].substr(2, parse[i].length) + "</h2>";
-    			}
+                    mrk += "<h2>" + parse[i].substr(2, parse[i].length) + "</h2>";
+                }
 
-    			// # H1
-    			else if (parse[i].substr(0,1) == "#") 
-    			{
+                // # H1
+                else if (parse[i].substr(0,1) == "#") 
+                {
                     if (listOpen)
                     {
                         mrk += "</ul>";
@@ -117,12 +119,12 @@ else
                         codeOpen = false;
                     }
 
-    				mrk += "<h1>" + parse[i].substr(1, parse[i].length) + "</h1>";
-    			}
+                    mrk += "<h1>" + parse[i].substr(1, parse[i].length) + "</h1>";
+                }
 
-    			// *bold* (need to be regex)
-    			else if (parse[i].substr(0,1) == "*" && parse[i].substr(-1) == "*")
-    			{
+                // *bold* (need to be regex)
+                else if (parse[i].substr(0,1) == "*" && parse[i].substr(-1) == "*")
+                {
                     if (listOpen)
                     {
                         mrk += "</ul>";
@@ -134,13 +136,13 @@ else
                         codeOpen = false;
                     }
 
-    				mrk += "<b>" + parse[i].substr(1, parse[i].length-1) + "</b>";
-    			}
+                    mrk += "<b>" + parse[i].substr(1, parse[i].length-1) + "</b>";
+                }
 
-    			//Alt-H1
-				//======
-				else if (parse[i+1] == this.md_str_repeat('=', parse[i].length))
-				{
+                //Alt-H1
+                //======
+                else if (parse[i+1] == this.md_str_repeat('=', parse[i].length))
+                {
                     if (listOpen)
                     {
                         mrk += "</ul>";
@@ -152,14 +154,14 @@ else
                         codeOpen = false;
                     }
 
-					mrk += "<h1>" + parse[i] + "</h1>";
-					parse[i+1]='';
-				}
+                    mrk += "<h1>" + parse[i] + "</h1>";
+                    parse[i+1]='';
+                }
 
-				//Alt-H2
-				//------
-				else if (parse[i+1] == this.md_str_repeat('-', parse[i].length))
-				{
+                //Alt-H2
+                //------
+                else if (parse[i+1] == this.md_str_repeat('-', parse[i].length))
+                {
                     if (listOpen)
                     {
                         mrk += "</ul>";
@@ -172,38 +174,38 @@ else
                     }
 
                     mrk += "<h2>" + parse[i] + "</h2>";
-					parse[i+1]='';
-				}
+                    parse[i+1]='';
+                }
 
-				// * unordered list
-    			else if (parse[i].substr(0,2) == "* ")
-    			{
-    				if(!listOpen)
-    				{
-    					mrk += "<ul>";
-    					listOpen = true;
-    				}
-    				mrk += "<li>" + parse[i].substr(2, parse[i].length) + "</li>";
-    			}
+                // * unordered list
+                else if (parse[i].substr(0,2) == "* ")
+                {
+                    if(!listOpen)
+                    {
+                        mrk += "<ul>";
+                        listOpen = true;
+                    }
+                    mrk += "<li>" + parse[i].substr(2, parse[i].length) + "</li>";
+                }
 
-    			// \s\s\s\s CODE
-    			else if (parse[i].substr(0,4) == "    ") //Code
-    			{
-    				if (!codeOpen)
-    				{
-    					mrk += "<style>pre.prettyprint{border:0 !important;}</style><div style='display:block;padding:9.5px;margin:0 0 10px;font-size:13px;line-height:20px;word-break:break-all;word-wrap:break-word;white-space:pre;white-space:pre-wrap;background-color:#f5f5f5;border:1px solid #ccc;border:1px solid rgba(0,0,0,0.15);-webkit-border-radius:4px;-moz-border-radius:4px;border-radius:4px;'><pre class=\"prettyprint\">";
-    					codeOpen = true;
-    				}
-    				mrk += "\r\n" + parse[i].substr(4,parse[i].length);	
-    			}
+                // \s\s\s\s CODE
+                else if (parse[i].substr(0,4) == "    ") //Code
+                {
+                    if (!codeOpen)
+                    {
+                        mrk += "<style>pre.prettyprint{border:0 !important;}</style><div style='display:block;padding:9.5px;margin:0 0 10px;font-size:13px;line-height:20px;word-break:break-all;word-wrap:break-word;white-space:pre;white-space:pre-wrap;background-color:#f5f5f5;border:1px solid #ccc;border:1px solid rgba(0,0,0,0.15);-webkit-border-radius:4px;-moz-border-radius:4px;border-radius:4px;'><pre class=\"prettyprint\">";
+                        codeOpen = true;
+                    }
+                    mrk += "\r\n" + parse[i].substr(4,parse[i].length);	
+                }
 
-    			// --- (HR)
-    			// *** (HR)
-    			// ___ (HR)
-    			else if (parse[i].substr(0,3) == "---" ||
-    				     parse[i].substr(0,3) == "***" ||
-						 parse[i].substr(0,3) == "___")
-    			{
+                // --- (HR)
+                // *** (HR)
+                // ___ (HR)
+                else if (parse[i].substr(0,3) == "---" ||
+                         parse[i].substr(0,3) == "***" ||
+                         parse[i].substr(0,3) == "___")
+                {
                     if (listOpen)
                     {
                         mrk += "</ul>";
@@ -215,12 +217,12 @@ else
                         codeOpen = false;
                     }
 
-    				mrk += "<hr />";
-    			}
+                    mrk += "<hr />";
+                }
 
-    			//Checkbox (un-checked)
-    			else if (parse[i].substr(0,4) == "- []") 
-    			{
+                //Checkbox (un-checked)
+                else if (parse[i].substr(0,4) == "- []") 
+                {
                     if (listOpen)
                     {
                         mrk += "</ul>";
@@ -233,12 +235,12 @@ else
                     }
 
                     mrk += "<br />";
-    				mrk += "<input type='checkbox' disabled=''>" + parse[i].substr(4,parse[i].length);		
-    			}
+                    mrk += "<input type='checkbox' disabled=''>" + parse[i].substr(4,parse[i].length);		
+                }
 
-    			//Checkbox (un-checked)
-    			else if (parse[i].substr(0,5) == "- [ ]") 
-    			{
+                //Checkbox (un-checked)
+                else if (parse[i].substr(0,5) == "- [ ]") 
+                {
                     if (listOpen)
                     {
                         mrk += "</ul>";
@@ -250,9 +252,9 @@ else
                         codeOpen = false;
                     }
 
-    				mrk += "<br />";
-    				mrk += "<input type='checkbox' disabled=''>" + parse[i].substr(5,parse[i].length);		
-    			}
+                    mrk += "<br />";
+                    mrk += "<input type='checkbox' disabled=''>" + parse[i].substr(5,parse[i].length);		
+                }
 
                 // Color!
                 else if(parse[i].match(/\[c\=(.*)\](.*)\[\/c\]/g))
@@ -278,9 +280,9 @@ else
                     mrk += "<font color='" + color + "'>" + texts + "</font>";
                 }
 
-    			//Checkbox (checked)
-    			else if (parse[i].substr(0,5) == "- [x]") 
-    			{
+                //Checkbox (checked)
+                else if (parse[i].substr(0,5) == "- [x]") 
+                {
                     if (listOpen)
                     {
                         mrk += "</ul>";
@@ -292,30 +294,30 @@ else
                         codeOpen = false;
                     }
 
-    				mrk += "<br />";
-    				mrk += "<input type='checkbox' checked='' disabled=''>" + parse[i].substr(5,parse[i].length);
-    			}
+                    mrk += "<br />";
+                    mrk += "<input type='checkbox' checked='' disabled=''>" + parse[i].substr(5,parse[i].length);
+                }
 
                 // Something we don't know.
-    			else
-    			{
-    				if (listOpen)
-    				{
-    					mrk += "</ul>";
-    					listOpen = false;
-    				}
-    				if (codeOpen)
-    				{
-    					mrk += "</pre></div>";
-    					codeOpen = false;
-    				}
-    				mrk += parse[i];
-    			}
-    		};
+                else
+                {
+                    if (listOpen)
+                    {
+                        mrk += "</ul>";
+                        listOpen = false;
+                    }
+                    if (codeOpen)
+                    {
+                        mrk += "</pre></div>";
+                        codeOpen = false;
+                    }
+                    mrk += parse[i];
+                }
+            };
 
-    		console.log(mrk);
-    		md.innerHTML = mrk;
-    	}
+            console.log(mrk);
+            md.innerHTML = mrk;
+        }
     }
 
     _('.md').markdown();
