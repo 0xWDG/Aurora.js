771712fd675f2ff302f2567de5e41d90d97f7e40
Wesley de Groot <wes@vista.aero>
2015-10-17 00:37:08 +0200

    added latest minified

--- latest/finish.sh
+++ latest/finish.sh
@@ -1,14 +1,9 @@
-# if exists remove old files
-rm GenerateDocs.php &>/dev/null
-
-cp ../_.js_data/GenerateDocs.php GenerateDocs.php &>/dev/null
-
-# Generate index.html & generate wiki -> function list.
-php GenerateDocs.php &>/dev/null
+mkdir m &> /dev/null
+mkdir modules &> /dev/null
 
 # Generate sourcemapping files.
 for i in *.js; 
-	do java -jar ../_.js_data/compiler.jar --js ./${i} --create_source_map ./m/${i}.map --js_output_file ./m/${i}; # Show errors ;D 
+	do java -jar ../_.js_data/compiler.jar --js ./${i} --create_source_map ./m/${i}.map --js_output_file ./m/${i} --language_in=ES5 &> /dev/null;
 done
 
 # Add: //# sourceMappingURL=/path/to/file.js.map
@@ -16,10 +11,20 @@ for i in *.js;
 	do sed -i -e '1i\'$'\n''//# sourceMappingURL='${i}'.map'$'\n' m/${i} &>/dev/null;
 done
 
-cd m &>/dev/null
-rm *.js-e &>/dev/null
-cd .. &>/dev/null
-rm GenerateDocs.php &>/dev/null
+rm m/*.js*e
+
+cd modules
+for i in *.js; 
+	do java -jar ../../_.js_data/compiler.jar --js ./${i} --create_source_map ./m/${i}.map --js_output_file ./m/${i} --language_in=ES5 &> /dev/null; # Show errors ;D 
+done
+
+# Add: //# sourceMappingURL=/path/to/file.js.map
+for i in *.js;
+	do sed -i -e '1i\'$'\n''//# sourceMappingURL='${i}'.map'$'\n' m/${i} &>/dev/null;
+done
+
+rm m/*.js*e
+cd ..
 
 # GOOD.
 /Applications/terminal-notifier.app/Contents/MacOS/terminal-notifier -appIcon "/Users/Wes/Cloud/WDGWV/Administatief/Logo's/logo.png" -contentImage "/Users/Wes/Cloud/Images/icons/-good.png" -title WDGWV -subtitle Success -message "Created Sourcemaps!" &> /dev/null
