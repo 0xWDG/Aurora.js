ef8a4e053a35d58afae3db4266db04313612d2d1
Wesley de Groot <wes@vista.aero>
2015-10-17 16:09:11 +0200

    Fix v0.0.5

--- latest/_.js
+++ latest/_.js
@@ -172,7 +172,7 @@
                             script.onload               = Callback;
                         
                         // catch loading error
-                            script.onerror              = 'this.onerror=null;this.src=\'https://raw.githubusercontent.com/wesdegroot/_.js/master/latest/modules/' + this.src +'\';';
+                            script.onerror              = 'this.onerror=null;this.src=\'https://raw.githubusercontent.com/wesdegroot/_.js/master/latest/modules/' + jsArray[i].toLowerCase() +'\';';
                     
                         document.head.appendChild(script);
                     }
