a1226370d521b84f8d0e913147ae0517a16d5ead
Wesley de Groot <wes@vista.aero>
2015-10-26 21:22:31 +0100

    Auto-Pushing v0.0.7

--- _.js_data/GenerateDocs.php
+++ _.js_data/GenerateDocs.php
@@ -126,8 +126,9 @@ for ($i=0; $i < sizeof($rettest[2]); $i++)
 			// * FUNCTION DEFINITION, TEXT BLA BLA
 
 			echo $ExplodeDataNow[$x] . PHP_EOL;
-			
-			$ExplodeDataNow[$x] = preg_replace("/\*\s/", null, $ExplodeDataNow[$x]);
+			$ExplodeDataNow[$x] = preg_replace("/\*\s+/", null, $ExplodeDataNow[$x]);
+			echo $ExplodeDataNow[$x] . PHP_EOL;
+
 			if ( $x > 2) 
 				$functions[$rettest[1][$i]]['text'][] = $ExplodeDataNow[$x];
 		}
