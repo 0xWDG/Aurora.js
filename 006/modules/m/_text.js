24f468d26441ed16a1c8ed2b8eca0c5e460520a7
Wesley de Groot <wes@vista.aero>
2015-10-30 23:39:14 +0100

    Auto-Pushing v0.0.6

--- 006/modules/m/_text.js
+++ 006/modules/m/_text.js
@@ -1,4 +1,2 @@
 //# sourceMappingURL=_text.js.map
-//# sourceMappingURL=_text.js.map
-//# sourceMappingURL=_text.js.map
 window._?_.fn.text_to_console=function(a){for(a=this.length;a--;)console.log("Text to console: "+this[a].innerHTML)}:alert("MISSING SOMETHING!");
