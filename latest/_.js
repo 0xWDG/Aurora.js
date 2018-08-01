91a88ede7416b455308f086ffa8ab1d341c5ba89
Wesley De Groot <wes@vista.aero>
2018-08-01 16:32:19 +0200

    Update _.js

--- latest/_.js
+++ latest/_.js
@@ -1206,7 +1206,7 @@
               // JavaScript Fix!
               var js = change.getElementsByTagName('script')
               for (var i = 0, j = js.length; i < j; i++) {
-                _.evaluate(js[i].innerHTML) // Find a better solution.
+                _.evaluate(js[i].innerHTML)
               }
 
               // fix posts also (.ajax)
@@ -1262,7 +1262,7 @@
               // JavaScript Fix!
               var js = change.getElementsByTagName('script')
               for (var i = 0, j = js.length; i < j; i++) {
-                _.evaluate(js[i].innerHTML) // Find a better solution
+                _.evaluate(js[i].innerHTML)
               }
 
               // fix posts also (.ajax)
@@ -2110,9 +2110,10 @@ if (typeof exports === 'undefined' && typeof document.createEvent !== 'undefined
   window.dispatchEvent(_JSLoaded)
 }
 
-// Please. please.
-// Sometimes we'll need to use eslint-disable-line
-// because eslint does not know what we're doing with the code.
+// Congrats,
+// You've readed a lot.
+// If you want to help
+// -> https://github.com/wdg/_.js/issues
 //
 // Thanks,
 // For reading the full source code.
